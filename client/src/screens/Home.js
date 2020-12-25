a587863ff18cd9c9e7a4acf04c0ecea8a8ffc34b
Ritesh Patil <riteshsp2000@gmail.com>
2020-12-25 20:31:48 +0530

    Fix Footer import error

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -6,7 +6,7 @@ import { Card } from '@material-ui/core';
 
 //Components
 import Header from '../components/marginals/Header';
-import Footer from '../components/marginals/Footer';
+import Footer from '../components/marginals/BottomBar';
 import TopBar from '../components/marginals/TopBar';
 import ArticleCard from '../components/widgets/ArticleCard';
 import Pulse from '../components/widgets/Pulse';
@@ -15,7 +15,6 @@ import FeaturedArticles from '../components/homepage/FeaturedArticles';
 import Trending from '../components/homepage/Trending';
 import Banner from '../components/homepage/Banner';
 import Calendar from '../assets/images/calendar.png';
-
 
 function Home() {
   return (
