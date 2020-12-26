4ecea1260c9fb49b78e101f778be509a3549e939
Ritesh Patil <riteshsp2000@gmail.com>
2020-12-26 13:54:39 +0530

    Modify the documentation config

---
+++ docs/website/docusaurus.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // ...
+  themes: ['@docusaurus/theme-classic', '@docusaurus/theme-live-codeblock'],
+};
--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -69,7 +69,7 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: 'dark',
   },
 
   // Add custom scripts here that would be placed in <script> tags.
