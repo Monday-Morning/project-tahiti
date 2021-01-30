33d038f78efa49a1cea1b8444e88ac5ee57e60bc
Vishal Rana <vishal.ch.36@gmail.com>
2021-01-30 19:05:44 +0530

    Update Portfolio.js

--- client/src/screens/Portfolio.js
+++ client/src/screens/Portfolio.js
@@ -3,9 +3,6 @@ import React from 'react';
 // libraries
 
 // Components
-import Header from '../components/marginals/Header';
-import Footer from '../components/marginals/Footer';
-import TopBar from '../components/marginals/TopBar';
 import BackLink from '../components/portfolio/Backlink';
 import UserCard from '../components/portfolio/UserCard';
 import Works from '../components/portfolio/Works';
@@ -15,12 +12,9 @@ import Works from '../components/portfolio/Works';
 function Portfolio() {
   return (
     <div>
-      <TopBar />
-      <Header />
       <BackLink />
       <UserCard />
       <Works />
-      <Footer />
     </div>
   );
 }
