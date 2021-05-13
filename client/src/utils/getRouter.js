c80f63ee62749dbfe05dbf8b09c48ac9932d004f
Ritesh Patil <riteshsp2000@gmail.com>
2021-05-13 23:54:02 +0530

    Create Router object for routing and navigation
    Export an object from getRouter.js file. The object contains properties as per the route types where the value for each key is an array of object(containing details about the particular route)

--- client/src/utils/getRouter.js
+++ client/src/utils/getRouter.js
@@ -1,85 +1,374 @@
-const CATEGORIES = {
-  HOME: '/',
-  CAMPUS: '/campus',
-  CONNECT: '/connect',
-  DDCWC: '/ddcwc',
-  CAREER: '/career',
-  ALUMNI: '/alumni',
-  EXPRESSIONS: '/expressions',
-};
+import LINKS from './getLinks';
 
-const SUB_CATEGORIES = {
-  CAMPUS: {
-    ACADEMICS: `${CATEGORIES.CAMPUS}/academics`,
-    BUZZ: `${CATEGORIES.CAMPUS}/campusBuzz`,
-    FORUM: `${CATEGORIES.CAMPUS}/forum`,
-    STUDENT_ACTIVITIES: `${CATEGORIES.CAMPUS}/studentActivities`,
-    HALLS: `${CATEGORIES.CAMPUS}/halls`,
-    POLL_ANALYSIS: `${CATEGORIES.CAMPUS}/pollAnalysis`,
+const CATEGORIES = [
+  {
+    name: 'HOME',
+    shortName: LINKS.CATEGORIES.HOME.split('/')[1],
+    path: LINKS.CATEGORIES.HOME,
+    asyncRoutePath: './Home',
+    idNumber: 0,
+    exact: true,
   },
-  CONNECT: {
-    INTERVIEWS: `${CATEGORIES.CONNECT}/interviews`,
-    SAC_SPEAKS: `${CATEGORIES.CONNECT}/sacSpeaks`,
-    CGPA: `${CATEGORIES.CONNECT}/cgpa`,
-    GUEST_INTERVIEW: `${CATEGORIES.CONNECT}/guestInterview`,
+  {
+    name: 'Campus',
+    shortName: LINKS.CATEGORIES.CAMPUS.split('/')[1],
+    path: LINKS.CATEGORIES.CAMPUS,
+    asyncRoutePath: './Category',
+    idNumber: 1,
+    exact: true,
   },
-  DDCWC: {
-    DIRECTORS_DESK: `${CATEGORIES.DDCWC}/directorsDesk`,
-    CHIEF_WARDENS_DESK: `${CATEGORIES.DDCWC}/chiefWardensDesk`,
+  {
+    name: 'Connect',
+    shortName: LINKS.CATEGORIES.CONNECT.split('/')[1],
+    path: LINKS.CATEGORIES.CONNECT,
+    asyncRoutePath: './Category',
+    idNumber: 2,
+    exact: true,
   },
-  CAREER: {
-    PLACEMENTS: `${CATEGORIES.CAREER}/placements`,
-    INTERNSHIPS: `${CATEGORIES.CAREER}/internships`,
-    HIGHER_EDUCATION: `${CATEGORIES.CAREER}/higherEducation`,
+  {
+    name: 'DD & CWC',
+    shortName: LINKS.CATEGORIES.DDCWC.split('/')[1],
+    path: LINKS.CATEGORIES.DDCWC,
+    asyncRoutePath: './Category',
+    idNumber: 3,
+    exact: true,
   },
-  ALUMNI: {
-    ALUMNUS_SPEAKS: `${CATEGORIES.ALUMNI}/alumnusSpeaks`,
-    ALUMNI_AFFAIRS: `${CATEGORIES.ALUMNI}/alumniAffairs`,
+  {
+    name: 'Career',
+    shortName: LINKS.CATEGORIES.CAREER.split('/')[1],
+    path: LINKS.CATEGORIES.CAREER,
+    asyncRoutePath: './Category',
+    idNumber: 4,
+    exact: true,
   },
-  EXPRESSIONS: {
-    WITSDOM: `${CATEGORIES.EXPRESSIONS}/witsdom`,
-    EDITORIAL: `${CATEGORIES.EXPRESSIONS}/editorial`,
-    PODCASTS: `${CATEGORIES.EXPRESSIONS}/podcasts`,
-    PHOTOSTORY: `${CATEGORIES.EXPRESSIONS}/photostory`,
-    GALLERY: `${CATEGORIES.EXPRESSIONS}/gallery`,
+  {
+    name: 'Alumni',
+    shortName: LINKS.CATEGORIES.ALUMNI.split('/')[1],
+    path: LINKS.CATEGORIES.ALUMNI,
+    asyncRoutePath: './Category',
+    idNumber: 5,
+    exact: true,
   },
-};
+  {
+    name: 'Expressions',
+    shortName: LINKS.CATEGORIES.EXPRESSIONS.split('/')[1],
+    path: LINKS.CATEGORIES.EXPRESSIONS,
+    asyncRoutePath: './Expressions',
+    idNumber: 1,
+    exact: true,
+  },
+];
 
-const INDIVIDUALS = {
-  ARTICLE: '/article/:id/:title',
-  WITSDOM: '/witsdom/:id/:title',
-  GALLERY: '/gallery/:id/:title',
-  PHOTOSTORY: '/photostory/:id/:title',
-  PORTFOLIO: '/portfolio/:id/:name',
-};
+const CAMPUS = [
+  {
+    name: 'Academics',
+    shortName: LINKS.SUB_CATEGORIES.CAMPUS.ACADEMICS.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CAMPUS.ACADEMICS,
+    asyncRoutePath: './SubCategory',
+    idNumber: 11,
+    exact: true,
+  },
+  {
+    name: 'Campus Buzz',
+    shortName: LINKS.SUB_CATEGORIES.CAMPUS.BUZZ.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CAMPUS.BUZZ,
+    asyncRoutePath: './SubCategory',
+    idNumber: 12,
+    exact: true,
+  },
+  {
+    name: 'Forum',
+    shortName: LINKS.SUB_CATEGORIES.CAMPUS.FORUM.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CAMPUS.FORUM,
+    asyncRoutePath: './SubCategory',
+    idNumber: 13,
+    exact: true,
+  },
+  {
+    name: 'Student Activities',
+    shortName: LINKS.SUB_CATEGORIES.CAMPUS.STUDENT_ACTIVITIES.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CAMPUS.STUDENT_ACTIVITIES,
+    asyncRoutePath: './SubCategory',
+    idNumber: 14,
+    exact: true,
+  },
+  {
+    name: 'Halls',
+    shortName: LINKS.SUB_CATEGORIES.CAMPUS.HALLS.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CAMPUS.HALLS,
+    asyncRoutePath: './SubCategory',
+    idNumber: 15,
+    exact: true,
+  },
+  {
+    name: 'Poll Analysis',
+    shortName: LINKS.SUB_CATEGORIES.CAMPUS.POLL_ANALYSIS.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CAMPUS.POLL_ANALYSIS,
+    asyncRoutePath: './SubCategory',
+    idNumber: 16,
+    exact: true,
+  },
+];
 
-// const INTERACTIONS = {
-//   CALENDAR: '/calendar',
-//   POLLS: '/polls',
-//   FORUMS: '/forums',
-// };
+const CONNECT = [
+  {
+    name: 'Interviews',
+    shortName: LINKS.SUB_CATEGORIES.CONNECT.INTERVIEWS.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CONNECT.INTERVIEWS,
+    asyncRoutePath: './SubCategory',
+    idNumber: 21,
+    exact: true,
+  },
+  {
+    name: 'Sac Speaks',
+    shortName: LINKS.SUB_CATEGORIES.CONNECT.SAC_SPEAKS.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CONNECT.SAC_SPEAKS,
+    asyncRoutePath: './SubCategory',
+    idNumber: 22,
+    exact: true,
+  },
+  {
+    name: 'CGPA',
+    shortName: LINKS.SUB_CATEGORIES.CONNECT.CGPA.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CONNECT.CGPA,
+    asyncRoutePath: './SubCategory',
+    idNumber: 23,
+    exact: true,
+  },
+  {
+    name: 'Guest Interview',
+    shortName: LINKS.SUB_CATEGORIES.CONNECT.GUEST_INTERVIEW.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CONNECT.GUEST_INTERVIEW,
+    asyncRoutePath: './SubCategory',
+    idNumber: 24,
+    exact: true,
+  },
+];
 
-const GENERAL = {
-  ABOUT: '/about',
-  GUIDE: '/guide',
-  CONTACT: '/contact',
-  // ARCHIVE: '/archive',
-  // TERMS_POLICIES: '/terms&policies',
-  // SAC_CLUBS: '/sac&clubs',
-  // HEALTH: '/health',
-  // EMERGENCY: '/emergency',
-};
+const DDCWC = [
+  {
+    name: "Director's Desk",
+    shortName: LINKS.SUB_CATEGORIES.DDCWC.DIRECTORS_DESK.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.DDCWC.DIRECTORS_DESK,
+    asyncRoutePath: './SubCategory',
+    idNumber: 31,
+    exact: true,
+  },
+  {
+    name: "Chief Warden's Desk",
+    shortName: LINKS.SUB_CATEGORIES.DDCWC.CHIEF_WARDENS_DESK.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.DDCWC.CHIEF_WARDENS_DESK,
+    asyncRoutePath: './SubCategory',
+    idNumber: 32,
+    exact: true,
+  },
+];
 
-const USER = {
-  PROFILE: '/userProfile',
-};
+const CAREER = [
+  {
+    name: 'Placements',
+    shortName: LINKS.SUB_CATEGORIES.CAREER.PLACEMENTS.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CAREER.PLACEMENTS,
+    asyncRoutePath: './SubCategory',
+    idNumber: 41,
+    exact: true,
+  },
+  {
+    name: 'Internships',
+    shortName: LINKS.SUB_CATEGORIES.CAREER.INTERNSHIPS.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CAREER.INTERNSHIPS,
+    asyncRoutePath: './SubCategory',
+    idNumber: 42,
+    exact: true,
+  },
+  {
+    name: 'Higher Education',
+    shortName: LINKS.SUB_CATEGORIES.CAREER.HIGHER_EDUCATION.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.CAREER.HIGHER_EDUCATION,
+    asyncRoutePath: './SubCategory',
+    idNumber: 43,
+    exact: true,
+  },
+  // {
+  //   name: "Live",
+  //   shortName: LINKS.SUB_CATEGORIES.CAREER.LIVE.split('/')[1],
+  //   path: LINKS.SUB_CATEGORIES.CAREER.LIVE,
+  //   asyncRoutePath: './SubCategory',
+  //   idNumber: 21,
+  //   exact: true,
+  // },
+];
 
-// eslint-disable-next-line import/prefer-default-export
-export const LINKS = Object.freeze({
+const ALUMNI = [
+  {
+    name: 'Alumnus Speaks',
+    shortName: LINKS.SUB_CATEGORIES.ALUMNI.ALUMNUS_SPEAKS.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.ALUMNI.ALUMNUS_SPEAKS,
+    asyncRoutePath: './SubCategory',
+    idNumber: 51,
+    exact: true,
+  },
+  {
+    name: 'Alumni Affairs',
+    shortName: LINKS.SUB_CATEGORIES.ALUMNI.ALUMNI_AFFAIRS.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.ALUMNI.ALUMNI_AFFAIRS,
+    asyncRoutePath: './SubCategory',
+    idNumber: 52,
+    exact: true,
+  },
+];
+
+const EXPRESSIONS = [
+  {
+    name: 'Witsdom',
+    shortName: LINKS.SUB_CATEGORIES.EXPRESSIONS.WITSDOM.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.EXPRESSIONS.WITSDOM,
+    asyncRoutePath: './SubCategory',
+    idNumber: 61,
+    exact: true,
+  },
+  {
+    name: 'Photostory',
+    shortName: LINKS.SUB_CATEGORIES.EXPRESSIONS.PHOTOSTORY.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.EXPRESSIONS.PHOTOSTORY,
+    asyncRoutePath: './SubCategory',
+    idNumber: 62,
+    exact: true,
+  },
+  {
+    name: 'Gallery',
+    shortName: LINKS.SUB_CATEGORIES.EXPRESSIONS.GALLERY.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.EXPRESSIONS.GALLERY,
+    asyncRoutePath: './SubCategory',
+    idNumber: 63,
+    exact: true,
+  },
+  {
+    name: 'Editorial',
+    shortName: LINKS.SUB_CATEGORIES.EXPRESSIONS.EDITORIAL.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.EXPRESSIONS.EDITORIAL,
+    asyncRoutePath: './SubCategory',
+    idNumber: 64,
+    exact: true,
+  },
+  {
+    name: 'Podcasts',
+    shortName: LINKS.SUB_CATEGORIES.EXPRESSIONS.PODCASTS.split('/')[1],
+    path: LINKS.SUB_CATEGORIES.EXPRESSIONS.PODCASTS,
+    asyncRoutePath: './Podcast',
+    idNumber: 65,
+    exact: true,
+  },
+];
+
+const INDIVIDUALS = [
+  {
+    name: 'Article',
+    shortName: LINKS.INDIVIDUALS.ARTICLE.split('/')[1],
+    path: LINKS.INDIVIDUALS.ARTICLE,
+    asyncRoutePath: './Article',
+    idNumber: 100,
+    exact: true,
+  },
+  {
+    name: 'Witsdom',
+    shortName: LINKS.INDIVIDUALS.WITSDOM.split('/')[1],
+    path: LINKS.INDIVIDUALS.WITSDOM,
+    asyncRoutePath: './Article',
+    idNumber: 101,
+    exact: true,
+  },
+  {
+    name: 'Photostory',
+    shortName: LINKS.INDIVIDUALS.PHOTOSTORY.split('/')[1],
+    path: LINKS.INDIVIDUALS.PHOTOSTORY,
+    asyncRoutePath: './PhotoStory',
+    idNumber: 102,
+    exact: true,
+  },
+  {
+    name: 'Gallery',
+    shortName: LINKS.INDIVIDUALS.GALLERY.split('/')[1],
+    path: LINKS.INDIVIDUALS.GALLERY,
+    asyncRoutePath: './Gallery',
+    idNumber: 103,
+    exact: true,
+  },
+  {
+    name: 'Portfolio',
+    shortName: LINKS.INDIVIDUALS.PORTFOLIO.split('/')[1],
+    path: LINKS.INDIVIDUALS.PORTFOLIO,
+    asyncRoutePath: './Portfolio',
+    idNumber: 104,
+    exact: true,
+  },
+];
+
+// TODO: Add the route objects for Interactions: Calendar, Polls, Forums Pages.
+const INTERACTIONS = [];
+
+const GENERAL = [
+  {
+    name: 'About',
+    shortName: LINKS.GENERAL.ABOUT.split('/')[1],
+    path: LINKS.GENERAL.ABOUT,
+    asyncRoutePath: './SubCategory',
+    idNumber: 201,
+    exact: true,
+  },
+  {
+    name: 'Guide',
+    shortName: LINKS.GENERAL.GUIDE.split('/')[1],
+    path: LINKS.GENERAL.GUIDE,
+    asyncRoutePath: './SubCategory',
+    idNumber: 202,
+    exact: true,
+  },
+  {
+    name: 'About',
+    shortName: LINKS.GENERAL.CONTACT.split('/')[1],
+    path: LINKS.GENERAL.CONTACT,
+    asyncRoutePath: './SubCategory',
+    idNumber: 203,
+    exact: true,
+  },
+  // TODO: Add the remaining general routes: Archive, Terms & Policies, SAC and Clubs, Health, Emergency
+];
+
+const USER = [
+  {
+    name: 'Onboarding',
+    shortName: LINKS.USER.ONBOARDING.split('/')[1],
+    path: LINKS.USER.ONBOARDING,
+    asyncRoutePath: './Onboarding',
+    idNumber: 301,
+    exact: true,
+  },
+  {
+    name: 'User',
+    shortName: LINKS.USER.PROFILE.split('/')[1],
+    path: LINKS.USER.PROFILE,
+    asyncRoutePath: './UserProfile',
+    idNumber: 301,
+    exact: true,
+  },
+];
+
+export default Object.freeze({
   CATEGORIES,
-  SUB_CATEGORIES,
+  SUB_CATEGORIES: {
+    ARRAY: [CAMPUS, CONNECT, DDCWC, CAREER, ALUMNI, EXPRESSIONS],
+    OBJECT: {
+      CAMPUS,
+      CONNECT,
+      DDCWC,
+      CAREER,
+      ALUMNI,
+      EXPRESSIONS,
+    },
+  },
   INDIVIDUALS,
-  // INTERACTIONS,
+  INTERACTIONS,
   GENERAL,
   USER,
 });
