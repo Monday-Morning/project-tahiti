f7cfc0ce1fac5d3deb125ee3107010128d208cd0
Ritesh Patil <riteshsp2000@gmail.com>
2021-05-13 12:53:33 +0530

    Add instagram grid on homepage

--- client/src/components/homepage/SocialMedia.js
+++ client/src/components/homepage/SocialMedia.js
@@ -4,7 +4,28 @@ import React from 'react';
 import { Button, makeStyles, Typography, Grid, Paper } from '@material-ui/core';
 import { Youtube, Instagram } from 'react-feather';
 
-// const YOUTUBE_LINKS = ['Link1', 'Link2', 'Link3'];
+// Assets
+import insta1 from '../../assets/images/instagram/insta1.jpeg';
+import insta2 from '../../assets/images/instagram/insta2.jpeg';
+import insta3 from '../../assets/images/instagram/insta3.jpeg';
+import insta4 from '../../assets/images/instagram/insta4.jpeg';
+import insta5 from '../../assets/images/instagram/insta5.jpeg';
+import insta6 from '../../assets/images/instagram/insta6.jpeg';
+import insta7 from '../../assets/images/instagram/insta7.jpeg';
+import insta8 from '../../assets/images/instagram/insta8.jpeg';
+import insta9 from '../../assets/images/instagram/insta9.jpeg';
+
+const INSTA_LINKS = [
+  insta1,
+  insta2,
+  insta3,
+  insta4,
+  insta5,
+  insta6,
+  insta7,
+  insta8,
+  insta9,
+];
 
 const Header = ({ Icon, title, buttonText, onClick }) => {
   const classes = useStyles();
@@ -35,6 +56,17 @@ const Header = ({ Icon, title, buttonText, onClick }) => {
 const Socials = () => {
   const classes = useStyles();
 
+  const openYoutube = () =>
+    window.open(
+      'https://www.instagram.com/mondaymorningnitrofficial',
+      '_blank',
+    );
+  const openInstagram = () =>
+    window.open(
+      'https://www.instagram.com/mondaymorningnitrofficial/',
+      '_blank',
+    );
+
   return (
     <div className={classes.root}>
       <Grid container spacing={4}>
@@ -44,13 +76,30 @@ const Socials = () => {
               Icon={Instagram}
               title='mondaymorningnitr'
               buttonText='Follow'
-              onClick={() =>
-                window.open(
-                  'https://www.instagram.com/mondaymorningnitrofficial',
-                  '_blank',
-                )
-              }
+              onClick={openInstagram}
             />
+
+            <Grid container spacing={3}>
+              {INSTA_LINKS.map((link) => (
+                <Grid
+                  key={link}
+                  item
+                  xs={4}
+                  className={classes.instaImageContainer}
+                >
+                  <img
+                    src={link}
+                    className={classes.instaImage}
+                    alt='Monday Morning Instagram'
+                    onClick={openInstagram}
+                    onKeyDown={openInstagram}
+                    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
+                    role='button'
+                    tabIndex='0'
+                  />
+                </Grid>
+              ))}
+            </Grid>
           </Paper>
         </Grid>
 
@@ -60,13 +109,9 @@ const Socials = () => {
               Icon={Youtube}
               title='Monday Morning'
               buttonText='Subscribe'
-              onClick={() =>
-                window.open(
-                  'https://www.youtube.com/channel/UCC2H5dnJ0AD-y8Fcscdq6bA',
-                  '_blank',
-                )
-              }
+              onClick={openYoutube}
             />
+            {/* <div className={classes.iframeContainer}> */}
             <iframe
               width='520'
               height='292'
@@ -100,6 +145,7 @@ const Socials = () => {
                 className={classes.smallVideo}
               />
             </div>
+            {/* </div> */}
           </Paper>
         </Grid>
       </Grid>
@@ -160,9 +206,16 @@ const useStyles = makeStyles(() => ({
     textAlign: 'center',
     textTransform: 'capitalize',
   },
+  iframeContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    height: '100%',
+  },
   largeVideo: {
     width: '100%',
-    height: '300px',
+    height: '66%',
     borderRadius: '10px',
     marginBottom: '10px',
   },
@@ -174,7 +227,20 @@ const useStyles = makeStyles(() => ({
   },
   smallVideo: {
     width: '48%',
-    height: '140px',
+    height: '33%',
     borderRadius: '10px',
+  },
+  instaImage: {
+    width: '100%',
+    height: 'auto',
+  },
+  instaImageContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    '&:hover': {
+      cursor: 'pointer',
+    },
   },
 }));
