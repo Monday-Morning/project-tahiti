10e32fb8c85126d94f64e7923c0d3bd8381f4e1a
Ritesh Patil <riteshsp2000@gmail.com>
2021-05-26 01:17:25 +0530

    fix: Add logic to determine articles on home page
    - Fetch the articles of 2 latest issues and use them to render the data on home page
    
    - Update youtube link instead of instagram for openYoutube function under Home/Socials component

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -17,26 +17,50 @@ import ActivityIndicator from '../components/shared/ActivityIndicator';
 import SocialMedia from '../components/homepage/SocialMedia';
 
 // Queries
-import GetIssueByIdQuery from '../graphql/queries/getIssueByID';
+// import GetIssueByIdQuery from '../graphql/queries/getIssueByID';
+import GetLatestIssue from '../graphql/queries/getLatestIssue';
 
 function Home() {
-  const { loading, error, data } = useQuery(GetIssueByIdQuery, {
-    variables: { id: '60a5913ec5df3a6218fa8336' },
+  const { loading, error, data } = useQuery(GetLatestIssue, {
+    variables: { limit: 3 },
   });
 
   if (loading && !data) return <ActivityIndicator size={150} />;
   if (error) return <div>{JSON.stringify(error)}</div>;
 
-  const {
-    getIssueByID: { articles, featured },
-  } = data;
+  const { listIssues: issues } = data;
+
+  const latestIssue = issues[0];
+  const secondLatestIssue = issues[1];
+
+  const { featured } = latestIssue;
+  let firstArticleStack;
+  let secondArticleStack;
+
+  // Logic to determine the content of First and Second article stack from the 2 latest issues
+  if (latestIssue.articles.length > 0 && latestIssue.articles.length < 3) {
+    firstArticleStack = secondLatestIssue.featured.slice(0, 3);
+    secondArticleStack = [
+      ...secondLatestIssue.featured.slice(3, 5),
+      secondLatestIssue.articles[0],
+    ];
+  } else if (
+    latestIssue.articles.length >= 3 &&
+    latestIssue.articles.length < 6
+  ) {
+    firstArticleStack = latestIssue.articles.slice(0, 3);
+    secondArticleStack = secondLatestIssue.featured.slice(0, 3);
+  } else {
+    firstArticleStack = latestIssue.articles.slice(0, 3);
+    secondArticleStack = latestIssue.articles.slice(3, 6);
+  }
 
   return (
     <>
       <Container>
         <FeaturedArticles articles={featured} />
         <Squiggles />
-        <ArticleCardStack articleList={articles.slice(0, 3)} />
+        <ArticleCardStack articleList={firstArticleStack} />
 
         <Grid container spacing={4} style={{ marginTop: 25 }}>
           <Grid item sm={8}>
@@ -47,7 +71,7 @@ function Home() {
           </Grid>
         </Grid>
 
-        <ArticleCardStack articleList={articles.slice(4, 7)} />
+        <ArticleCardStack articleList={secondArticleStack} />
       </Container>
 
       <Banner />
