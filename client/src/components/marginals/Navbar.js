d124306ef4221c869cfde6c1905d323c9d0b2b49
Ritesh Patil <riteshsp2000@gmail.com>
2021-07-26 12:52:27 +0530

    refactor: article card stack components and navbar componenets

--- client/src/components/marginals/Navbar.js
+++ client/src/components/marginals/Navbar.js
@@ -10,39 +10,8 @@ import MobileNavbar from './MobileNavbar';
 // Assets
 import theme from '../../config/themes/light';
 
-const NAV = [
-  {
-    to: '/',
-    name: 'Home',
-  },
-  {
-    to: '/campus',
-    name: 'Campus',
-  },
-  {
-    to: '/connect',
-    name: 'Connect',
-  },
-  {
-    to: '/ddcwc',
-    name: 'DD & CWC',
-  },
-  {
-    to: '/career',
-    name: 'Career',
-  },
-  {
-    to: '/alumni',
-    name: 'Alumni',
-  },
-  {
-    to: '/expressions',
-    name: 'Expressions',
-  },
-];
-
 const Header = () => {
   const matches = useMediaQuery(theme.breakpoints.up('md'));
-  return matches ? <DesktopNavbar /> : <MobileNavbar nav={NAV} />;
+  return matches ? <DesktopNavbar /> : <MobileNavbar />;
 };
 export default Header;
