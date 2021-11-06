a91632e0c2b2fc2b769ec5fe18ae6b06a4371f98
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-11-06 21:37:05 +0530

    fix: remove useLocation from getArticleLink

--- client/src/utils/getArticleLink.js
+++ client/src/utils/getArticleLink.js
@@ -1,30 +1,12 @@
-import { useLocation } from 'react-router-dom';
 // import ROUTES from './getRoutes';
 
 export const getArticleSlug = (title) =>
   title.replace(/\W/g, '-').replace(/-{2,}/g, '-');
 
 export default (id, title, typeObject = null) => {
-  let artilceTypeObject = typeObject;
-  const { pathname } = useLocation();
-
-  if (!typeObject) {
-    // const category = pathname.split('/')[1];
-    const subCategory = pathname.split('/')[2];
-    // const categoryName = ROUTES.CATEGORIES.filter(
-    //   ({ shortName }) => shortName === category,
-    // )[0].name;
-
-    artilceTypeObject = {
-      isWitsdom: subCategory === 'witsdom',
-      isPhotostory: subCategory === 'photostory',
-      isGallery: subCategory === 'gallery',
-    };
-  }
-  const { isWitsdom, isGallery, isPhotostory } = artilceTypeObject;
-
-  if (isWitsdom) return `/article/${id}/${getArticleSlug(title)}`;
-  if (isGallery) return `/gallery/${id}/${getArticleSlug(title)}`;
-  if (isPhotostory) return `/photostory/${id}/${getArticleSlug(title)}`;
+  if (typeObject?.isWitsdom) return `/article/${id}/${getArticleSlug(title)}`;
+  if (typeObject?.isGallery) return `/gallery/${id}/${getArticleSlug(title)}`;
+  if (typeObject?.isPhotostory)
+    return `/photostory/${id}/${getArticleSlug(title)}`;
   return `/article/${id}/${getArticleSlug(title)}`;
 };
