653f3bbd2470123141c6f729cc97bf1ea5825521
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-11-11 18:46:56 +0530

    fix: server render component styles

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -22,6 +22,14 @@ import lightTheme from '../config/themes/light';
 function TahitiApp({ Component, pageProps }) {
   const classes = useStyles();
 
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <ApolloClient>
       <Head>
