8eae7643a39498d3f1c4fe9465a39cee14d951d3
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-11-11 18:46:19 +0530

    feat: add _document for global icons

--- client/src/pages/_document.jsx
+++ client/src/pages/_document.jsx
@@ -1,11 +1,7 @@
+import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class TahitiDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html lang='en'>
