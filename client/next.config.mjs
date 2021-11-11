bd84404e43fb44825ff2e5aa60a33e2bf7057b4a
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-11-11 18:43:56 +0530

    chore: set build config for eslint
    temporarily ignore esling errors till end of migration

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -11,6 +11,11 @@ const nextConfig = (phase, { defaultConfig }) => ({
   images: {
     domains: ['ik.imagekit.io'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 });
 
 export default nextConfig;
