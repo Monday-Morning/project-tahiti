d44b46122cd40bfd673598cdb7ed9593132ca5fa
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-11-12 16:55:24 +0530

    fix(next): SEO, Build Config and Server Side Styling (#126)
    * chore: update webmanifest description
    
    * chore: set build config for eslint
    
    temporarily ignore esling errors till end of migration
    
    * feat: add sharp for image optimization
    
    * feat: add _document for global icons
    
    * fix: server render component styles
    
    * feat: add meta tags for SEO
    
    * fix: yarn lockfile mismatch
    
    * fix: invalid variable

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -22,6 +22,14 @@ import lightTheme from '../config/themes/light';
 function TahitiApp({ Component, pageProps }) {
   const classes = useStyles();
 
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <ApolloClient>
       <Head>
@@ -32,61 +40,6 @@ function TahitiApp({ Component, pageProps }) {
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1, shrink-to-fit=no'
-        />
-
-        {/* <!-- =============== Primary Meta Tags =============== --> */}
-        <title>Monday Morning</title>
-        <meta name='title' content='Monday Morning' />
-        <meta
-          name='description'
-          content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
-        />
-        <meta
-          name='keywords'
-          content='monday morning, mondaymorning, monday morning nit rourkela, nit, nit rourkela'
-        />
-
-        {/* <!-- =============== Open Graph / Facebook =============== --> */}
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://mondaymorning.nitrkl.ac.in/' />
-        <meta
-          property='og:site_name'
-          content='Monday Morning | The Student Media Body of NIT Rourkela, India'
-        />
-        <meta property='og:title' content='Monday Morning' />
-        <meta
-          property='og:description'
-          content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
-        />
-        <meta
-          property='og:image'
-          itemProp='image'
-          content='/icon-256x256.png'
-        />
-        <meta
-          property='og:image:url'
-          content='https://mondaymorning.nitrkl.ac.in/icon-256x256.png'
-        />
-        <meta
-          property='og:image:secure_url'
-          content='https://mondaymorning.nitrkl.ac.in/icon-256x256.png'
-        />
-        <meta property='og:image:type' content='image/png' />
-
-        {/* <!-- =============== Twitter =============== --> */}
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta
-          property='twitter:url'
-          content='https://mondaymorning.nitrkl.ac.in/'
-        />
-        <meta property='twitter:title' content='Monday Morning' />
-        <meta
-          property='twitter:image'
-          content='https://mondaymorning.nitrkl.ac.in/icon-256x256.png'
-        />
-        <meta
-          property='twitter:description'
-          content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
         />
       </Head>
       <ThemeProvider theme={lightTheme}>
