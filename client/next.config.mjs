207baadabf44b326ad4ad5b4af016a422e4914a0
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-02-27 14:14:01 +0530

    feat: configure dotenv with next config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,19 +1,32 @@
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js';
-import { DEV_ENV, PROD_ENV } from './.env.mjs';
+import dotenv from 'dotenv';
+import { PHASE_DEVELOPMENT_SERVER, PHASE_TEST } from 'next/constants.js';
+
+const isDev = (phase) =>
+  phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_TEST;
+
+const getEnv = (phase) => {
+  const { parsed } = dotenv.config({
+    path: `./env/.env.${isDev(phase) ? 'development' : 'production'}`,
+  });
+  return parsed;
+};
 
 const nextConfig = (phase, { defaultConfig }) => ({
   ...defaultConfig,
-  env: phase === PHASE_DEVELOPMENT_SERVER ? DEV_ENV : PROD_ENV,
-  reactStrictMode: phase === PHASE_DEVELOPMENT_SERVER,
+  reactStrictMode: !isDev(phase),
+  env: getEnv(phase),
   devIndicators: {
     buildActivityPosition: 'bottom-left',
   },
   images: {
-    domains: ['ik.imagekit.io'],
+    domains: ['ik.imagekit.io', 'mondaymorning.nitrkl.ac.in', 'mm.dashnet.in'],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+    /**
+     * Warning!
+     * This allows production builds to successfully complete even if your project has ESLint errors.
+     * Do not enable for production builds.
+     */
     ignoreDuringBuilds: true,
   },
 });
