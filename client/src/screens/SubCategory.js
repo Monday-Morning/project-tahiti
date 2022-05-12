266509e9f56866a5522c736231921e59a584ebf4
Aashish <ashishpradhan2345@gmail.com>
2022-05-12 19:12:14 +0530

    styles: equal number of articles on every page

--- client/src/screens/SubCategory.js
+++ client/src/screens/SubCategory.js
@@ -11,36 +11,18 @@ import Title from '../components/shared/PageTitle';
 import ArticleCardStack from '../components/widgets/article/ArticleCardStack';
 import BigArticleCard from '../components/widgets/article/BigArticleCard';
 
-function SubCategory({ categoryName, subCategoryDetails, articleList }) {
+function SubCategory({ categoryName, subCategoryDetails, articleList, totalPages, pageNo, handleChange }) {
 
   const classes = useStyles();
-
-  const [pageNo, setPageNo] = useState(1);
-
-  const handleChange = (event, value) => {
-    setPageNo(value);
-  };
-
-  const noOfarticles = articleList[0].length;
-  const maxNoofPages = Math.ceil((noOfarticles - 7) / 9) + 1;
 
   return (
     <Container>
       <BackLink backTo={categoryName} />
       <Title title={subCategoryDetails.name} />
-      {pageNo === 1 ? <>
-        <BigArticleCard article={articleList[0][0]} />
-        <ArticleCardStack articleList={articleList[0].slice(1, 4)} />
-        <ArticleCardStack articleList={articleList[0].slice(4, 7)} />
-      </>
-        :
-        <>
-          <ArticleCardStack articleList={articleList[0].slice(pageNo * 9 - 11, pageNo * 9 - 8)} />
-          <ArticleCardStack articleList={articleList[0].slice(pageNo * 9 - 8, pageNo * 9 - 5)} />
-          <ArticleCardStack articleList={articleList[0].slice(pageNo * 9 - 5, pageNo * 9 - 2)} />
-        </>
-      }
-      <Pagination className={classes.pagination} count={maxNoofPages} page={pageNo} onChange={handleChange} />
+      <BigArticleCard article={articleList[0][0]} />
+      <ArticleCardStack articleList={articleList[0].slice(1, 4)} />
+      <ArticleCardStack articleList={articleList[0].slice(4, 7)} />
+      <Pagination className={classes.pagination} count={totalPages} page={pageNo} onChange={handleChange} />
     </Container>
   );
 }
