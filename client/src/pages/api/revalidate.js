fee5d087b680363c079aaaae59f815793f6854e0
Aashish <83752052+120EE0692@users.noreply.github.com>
2022-07-02 13:46:36 +0530

    feat: add on demand ISR for article (#234)
    * feat: add on demand ISR for article
    
    * feat: add on demand ISR for all pages
    
    * chore: upgrade nextjs 12.1.0 to 12.2.0
    
    * wip: add stable revalidate

---
+++ client/src/pages/api/revalidate.js
@@ -0,0 +1,30 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res
+      .status(400)
+      .json({ error: 'Invalid HTTP method. Only POST requests are allowed.' });
+  }
+
+  if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  try {
+    const body = req.body;
+    if (!body) {
+      res.status(400).send('Bad request (no body)');
+      return;
+    }
+
+    const url = body.url;
+    if (url) {
+      await res.revalidate(`${url}`);
+      return res.json({
+        revalidate: true,
+        revalidatedURL: `https://mondaymorning.nitrkl.ac.in${url}`,
+      });
+    }
+  } catch (err) {
+    return res.status(500).send('Error revalidating:', err);
+  }
+}
