c7a17c296cec37e26a162e920e945b4553453494
Aashish <ashishpradhan2345@gmail.com>
2022-08-12 14:47:31 +0530

    chore: configure firebase

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,27 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-const configureFirebase = async () => {
-  if (process.env.NODE_ENV === 'production') {
-    const response = await fetch('/__/firebase/init.json');
-    const config = await response.json();
-    firebase.initializeApp(config);
-  } else {
-    const firebaseConfig = {
-      apiKey: process.env.APIKEY,
-      authDomain: process.env.AUTHDOMAIN,
-      databaseURL: process.env.DATABASEURL,
-      projectId: process.env.PROJECTID,
-      storageBucket: process.env.STORAGEBUCKET,
-      messagingSenderId: process.env.MESSAGINGSENDERID,
-      appId: process.env.APPID,
-      measurementId: process.env.MEASUREMENTID,
-    };
-
-    firebase.initializeApp(firebaseConfig);
-  }
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
 };
 
-export const { analytics } = firebase;
-
-export default configureFirebase;
+const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
