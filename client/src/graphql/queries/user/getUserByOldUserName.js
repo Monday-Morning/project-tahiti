b7ebcd9b4c01199a709446a046d311f4400b0313
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-08-27 16:35:51 +0530

    fix: requested changes

--- client/src/graphql/queries/user/getUserByOldUserName.js
+++ client/src/graphql/queries/user/getUserByOldUserName.js
@@ -1,20 +1,10 @@
 import { gql } from '@apollo/client';
 
 export default gql`
-  query GetUserByOldUserName($oldUserName: String!) {
-    getUserByOldUserName(oldUserName: $oldUserName) {
-      id
-      firstName
-      lastName
-      email
-      picture {
-        store
-        storePath
-      }
-      contributions {
-        model
-        reference
-      }
-    }
-  }
+	query GetUserByOldUserName($oldUserName: String!) {
+		getUserByOldUserName(oldUserName: $oldUserName) {
+			id
+			fullName
+		}
+	}
 `;
--- client/src/pages/portfolio/[...userId].jsx
+++ client/src/pages/portfolio/[...userId].jsx
@@ -9,185 +9,147 @@ import { GraphClient } from '../../config/ApolloClient';
 import getUserByOldUserName from '../../graphql/queries/user/getUserByOldUserName';
 import getUserById from '../../graphql/queries/user/getUserById';
 import getListOfArticles from '../../graphql/queries/article/getListOfArticles';
+import { getUserSlug } from '../../utils/getUserSlug';
 
 //Store
 import getStores from '../../utils/getStores';
 
-const PortfolioPage = ({
-  userId,
-  firstName,
-  lastName,
-  email,
-  profileImageLink,
-  articleList,
-  noOfArticle,
-  year,
-}) => {
-  const { isFallback } = useRouter();
+const PortfolioPage = ({ userId, firstName, lastName, email, profileImageLink, articleList, noOfArticle, year }) => {
+	const { isFallback } = useRouter();
 
-  const user = {
-    userId,
-    firstName,
-    lastName,
-    email,
-    profileImageLink,
-    articleList,
-    noOfArticle,
-    year,
-  };
+	const user = {
+		userId,
+		firstName,
+		lastName,
+		email,
+		profileImageLink,
+		articleList,
+		noOfArticle,
+		year,
+	};
 
-  return (
-    <>
-      <Marginals>{!isFallback && <Portfolio user={user} />}</Marginals>
-    </>
-  );
+	return (
+		<>
+			<Marginals>{!isFallback && <Portfolio user={user} />}</Marginals>
+		</>
+	);
 };
 
 export default PortfolioPage;
 
 export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: 'blocking',
-  };
+	return {
+		paths: [],
+		fallback: 'blocking',
+	};
 }
 
 export async function getStaticProps({
-  params: {
-    userId: [portfolioId, userName],
-  },
+	params: {
+		userId: [portfolioId, userSlug],
+	},
 }) {
-  /**** to bring the portfolio URL in correct format - start ****/
+	/**** to bring the portfolio URL in correct format - start ****/
 
-  let user;
+	if (!userName && typeof portfolioId === String) {
+		const {
+			data: {
+				getUserByOldUserName: { id, fullName },
+			},
+		} = await GraphClient.query({
+			query: getUserByOldUserName,
+			variables: {
+				oldUserName: portfolioId,
+			},
+		});
 
-  if (!userName && typeof portfolioId === String) {
-    user = await GraphClient.query({
-      query: getUserByOldUserName,
-      variables: {
-        oldUserName: portfolioId,
-      },
-    });
+		if (!id) {
+			return {
+				notFound: true,
+			};
+		}
 
-    let {
-      data: {
-        getUserByOldUserName: { id, firstName, lastName },
-      },
-    } = user;
+		return {
+			redirect: {
+				destination: `/portfolio/${id}/${getUserSlug(fullName)}`,
+				permanent: false,
+			},
+		};
+	}
 
-    if (!id) {
-      if (!user.data.getUserById.id) {
-        return {
-          notFound: true,
-        };
-      }
+	const {
+		data: { getUserById: user },
+	} = await GraphClient.query({
+		query: getUserById,
+		variables: {
+			getUserById: portfolioId,
+		},
+	});
 
-      firstName = user.data.getUserByID.firstName;
-      lastName = user.data.getUserByID.lastName;
-    }
+	if (!user) {
+		return {
+			notFound: true,
+		};
+	}
 
-    return {
-      redirect: {
-        destination: lastName
-          ? `/portfolio/${id}/${firstName.toLowerCase()}_${lastName.toLowerCase()}`
-          : `/portfolio/${id}/${firstName.toLowerCase()}}`,
-        permanent: false,
-      },
-    };
-  }
+	if (userSlug !== getUserSlug(user.fullName)) {
+		return {
+			redirect: {
+				destination: `/portfolio/${id}/${getUserSlug(fullName)}`,
+				permanent: false,
+			},
+		};
+	}
 
-  if (portfolioId) {
-    user = await GraphClient.query({
-      query: getUserById,
-      variables: {
-        getUserById: portfolioId,
-      },
-    });
+	/**** to bring the portfolio URL in correct format - end ****/
 
-    const _firstName = user.data.getUserByID?.firstName.toLowerCase();
-    const _lastName = user.data.getUserByID?.lastName.toLowerCase();
-    const _id = user.data.getUserByID?.id;
+	const { firstName, lastName, email, picture, contributions } = user;
 
-    if (
-      _id &&
-      ((_lastName && userName != `${_firstName}_${_lastName}`) ||
-        (!_lastName && userName != `${_firstName}`))
-    ) {
-      return {
-        redirect: {
-          destination: _lastName
-            ? `/portfolio/${_id}/${_firstName}_${_lastName}`
-            : `/portfolio/${_id}/${_firstName}`,
-          permanent: false,
-        },
-      };
-    } else if (!_id) {
-      return {
-        notFound: true,
-      };
-    }
-  }
+	const noProfilePage = ['Guest', 'Team'];
 
-  /**** to bring the portfolio URL in correct format - end ****/
+	if (noProfilePage.includes(firstName)) {
+		return {
+			notFound: true,
+		};
+	}
 
-  user = await GraphClient.query({
-    query: getUserById,
-    variables: {
-      getUserById: portfolioId,
-    },
-  });
+	const articleIdList = contributions
+		.filter(({ model }) => model === 'Article')
+		.reduce((prev, curr) => [...prev, curr.reference], []);
 
-  let {
-    data: {
-      getUserByID: { firstName, lastName, email, picture, contributions },
-    },
-  } = user;
+	const {
+		data: { getListOfArticles: articleList },
+	} = await GraphClient.query({
+		query: getListOfArticles,
+		variables: {
+			ids: articleIdList,
+			limit: 40,
+		},
+	});
 
-  const noProfilePage = ['Guest', 'Team'];
+	const noOfArticle = articleList.length;
 
-  if (noProfilePage.includes(firstName)) {
-    return {
-      notFound: true,
-    };
-  }
+	let { store, storePath } = picture;
+	let profileImageLink = getStores[store] + storePath;
 
-  const articleIdList = contributions
-    .filter(({ model }) => model === 'Article')
-    .reduce((prev, curr) => [...prev, curr.reference], []);
+	let year = ' ';
 
-  const {
-    data: { getListOfArticles: articleList },
-  } = await GraphClient.query({
-    query: getListOfArticles,
-    variables: {
-      ids: articleIdList,
-      limit: 40,
-    },
-  });
+	if (noOfArticle) {
+		year = articleList[noOfArticle - 1].createdAt.substring(0, 4);
+		year += '-' + (Number(year.substring(2)) + 1).toString();
+	}
 
-  const noOfArticle = articleList.length;
-
-  let { store, storePath } = picture;
-  let profileImageLink = getStores[store] + storePath;
-
-  let year = ' ';
-
-  if (noOfArticle) {
-    year = articleList[noOfArticle - 1].createdAt.substring(0, 4);
-    year += '-' + (Number(year.substring(2)) + 1).toString();
-  }
-
-  return {
-    props: {
-      userId: portfolioId,
-      firstName,
-      lastName,
-      email,
-      profileImageLink,
-      articleList,
-      noOfArticle,
-      year,
-    },
-    revalidate: 60 * 60 * 24 * 7,
-  };
+	return {
+		props: {
+			userId: portfolioId,
+			firstName,
+			lastName,
+			email,
+			profileImageLink,
+			articleList,
+			noOfArticle,
+			year,
+		},
+		revalidate: 60 * 60 * 24 * 7,
+	};
 }
