b09b39296165502c6cd201d64bb94ea08c6ca303
Aashish <83752052+120EE0692@users.noreply.github.com>
2022-08-27 22:18:53 +0530

    feat: replace old portfolio (#240)
    * feat: add query getUserByOldUserName
    
    * feat: add redirect page for old portfolio
    
    * fix: undefined memory address issue
    
    * chore: get id field
    
    * feat: redirect to new portfolio
    
    * feat: revalidate portfolio in 7 days
    
    * fix: requested changes
    
    * chore: query fullName
    
    * wip: redirect to new portfolio
    
    * feat: check portfolio id format
    
    Co-authored-by: Rutaj Dash <33367546+rutajdash@users.noreply.github.com>

---
+++ client/src/graphql/queries/user/getUserByOldUserName.js
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client';
+
+export default gql`
+  query GetUserByOldUserName($oldUserName: String!) {
+    getUserByOldUserName(oldUserName: $oldUserName) {
+      id
+      fullName
+    }
+  }
+`;
