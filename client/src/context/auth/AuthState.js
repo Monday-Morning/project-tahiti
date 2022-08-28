017d21f4b0e8f1f84e576a6d44ea96d485eed3f6
Aashish <ashishpradhan2345@gmail.com>
2022-08-28 15:54:30 +0530

    feat: set user firebaseToken and refreshToken

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -3,15 +3,48 @@ import React, { useState, useEffect } from 'react';
 //context
 import authContext from './AuthContext';
 
-import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
+import {
+  GoogleAuthProvider,
+  signInWithCredential,
+  getIdToken,
+  onAuthStateChanged,
+} from 'firebase/auth';
 import { auth } from '../../config/firebase';
+import { getApolloLink, GraphClient } from '../../config/ApolloClient';
+import { setCookie } from 'nookies';
 
 const AuthState = ({ children }) => {
+  const [refreshToken, setRefreshToken] = useState('');
+  const [user, setUser] = useState(null);
+  const [firebaseToken, setFirebaseToken] = useState('');
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (_user) => {
+      setUser(_user);
+      setFirebaseToken(await getIdToken(_user, false));
+      console.log('auth state changed', _user);
+    });
+  }, []);
+
+  useEffect(() => {
+    GraphClient.setLink(getApolloLink(firebaseToken));
+    setCookie(null, 'firebaseToken', firebaseToken, {
+      secure: true,
+      sameSite: true,
+      maxAge: 3600,
+    });
+    console.log('graph client auth', firebaseToken);
+  }, [firebaseToken]);
+
   const loginWithToken = (token) => {
     const credential = GoogleAuthProvider.credential(token);
 
     signInWithCredential(auth, credential)
-      .then((res) => console.log(res.user))
+      .then((res) => {
+        setUser(res.user);
+        setFirebaseToken(res.user.accessToken);
+        setRefreshToken(res.user.refreshToken);
+      })
       .catch((error) => {
         // Handle Errors here.
         const errorCode = error.code;
@@ -30,7 +63,9 @@ const AuthState = ({ children }) => {
   };
 
   return (
-    <authContext.Provider value={{ loginWithToken, logout }}>
+    <authContext.Provider
+      value={{ loginWithToken, logout, user, firebaseToken, refreshToken }}
+    >
       {children}
     </authContext.Provider>
   );
