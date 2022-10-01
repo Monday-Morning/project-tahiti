8f6ddc98c54356985ac7b551ffa86db9da007cf1
Anish Sarawgi <anishsarawgi6@gmail.com>
2022-10-02 00:27:13 +0530

    fix: modify meta tags

--- client/src/pages/archive.jsx
+++ client/src/pages/archive.jsx
@@ -14,7 +14,7 @@ function ArchivePage({}) {
     <>
       <Head>
         {/* <!-- =============== Primary Meta Tags =============== --> */}
-        <title>Contact | Monday Morning</title>
+        <title>Archive | Monday Morning</title>
         <meta name='title' content='Archive | Monday Morning' />
         <meta
           name='description'
@@ -29,13 +29,13 @@ function ArchivePage({}) {
         <meta property='og:type' content='website' />
         <meta
           property='og:url'
-          content='https://mondaymorning.nitrkl.ac.in/contact'
+          content='https://mondaymorning.nitrkl.ac.in/archive'
         />
         <meta
           property='og:site_name'
           content='Monday Morning | The Student Media Body of NIT Rourkela, India'
         />
-        <meta property='og:title' content='Contact | Monday Morning' />
+        <meta property='og:title' content='Archive | Monday Morning' />
         <meta
           property='og:description'
           content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
@@ -59,7 +59,7 @@ function ArchivePage({}) {
         <meta property='twitter:card' content='summary_large_image' />
         <meta
           property='twitter:url'
-          content='https://mondaymorning.nitrkl.ac.in/Contact'
+          content='https://mondaymorning.nitrkl.ac.in/archive'
         />
         <meta property='twitter:title' content='Monday Morning' />
         <meta
