343f3f7730504b62f9d46d0eca4825bbf2a42dcf
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-10-10 18:34:13 +0530

    fix: firebase env vars is optional during build

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -11,5 +11,8 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
-const firebaseApp = initializeApp(firebaseConfig);
-export const auth = getAuth(firebaseApp);
+const firebaseApp = !process.env.NEXT_PUBLIC_PROJECT_ID
+  ? initializeApp(firebaseConfig)
+  : null;
+
+export const auth = !firebaseApp ? getAuth(firebaseApp) : null;
