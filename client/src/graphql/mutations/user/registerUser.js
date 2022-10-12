6db1dd60d084463bf046dae83763e12fea64bf11
Aashish <83752052+120EE0692@users.noreply.github.com>
2022-10-12 20:01:29 +0530

    feat: user onboarding  (#261)
    * chore: yarn upgrade
    
    * chore: add imagekit imagekit-js
    
    * feat: add register user mutation
    
    * feat: add imagekit auth
    
    * feat: user onboarding
    
    * chore: remove imagekit authend point
    
    * docs: remove console log
    
    Co-authored-by: Rutaj Dash <33367546+rutajdash@users.noreply.github.com>

---
+++ client/src/graphql/mutations/user/registerUser.js
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+
+export default gql`
+  mutation registerUser($fullName: String!, $email: String!) {
+    registerUser(fullName: $fullName, email: $email) {
+      id
+      fullName
+      email
+    }
+  }
+`;
