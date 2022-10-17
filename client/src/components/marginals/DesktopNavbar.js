a39ce91bf39e44fb07d0b8b76fe6eaa53076254d
Mr-Proton <gavynpaulingee@gmail.com>
2022-10-17 15:48:38 +0530

    feat:Search bar added

--- client/src/components/marginals/DesktopNavbar.js
+++ client/src/components/marginals/DesktopNavbar.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+import TrendingUpSharpIcon from '@mui/icons-material/TrendingUpSharp';
 
 import makeStyles from '@mui/styles/makeStyles';
 import { Container, Typography, TextField } from '@mui/material';
@@ -11,11 +16,14 @@ import ROUTES from '../../utils/getRoutes';
 
 // Assets
 import logoFullBlack from '../../assets/images/logos/logo_full_black.png';
+import { height } from '@mui/system';
 
 const DesktopNavbar = () => {
   const classes = useStyles();
   const router = useRouter();
   const [search, setSearch] = useState('');
+  const [IsSearchActive, setIsSearchActive] = useState(false);
+  const [IsInputActive, setIsInputActive] = useState(false);
 
   // console.log(search);
 
@@ -32,54 +40,140 @@ const DesktopNavbar = () => {
     }
   };
 
+  const searchActive = () => {
+    setIsInputActive();
+    setIsSearchActive((current) => !current);
+  };
+
+  const inputActive = () => {
+    setIsInputActive((current) => !current);
+  };
+
   return (
-    <Container>
-      <nav
-        aria-label='Monday Morning Navigation'
-        className={classes.navContainer}
+    <>
+      <div
+        className={classes.searchMenu}
+        style={{
+          display: IsSearchActive ? 'block' : 'none',
+        }}
       >
-        <div
-          aria-label='Details Container'
-          className={classes.detailsContainer}
+        <div className={classes.blackBackground} onClick={searchActive}></div>
+        <div className={classes.searchBar}>
+          <div className={classes.searchBox}>
+            <TextField
+              className={classes.searchField}
+              id='input-with-icon-textfield'
+              placeholder='Search for articles'
+              onClick={setIsInputActive}
+              color='primary'
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='start'>
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+              variant='standard'
+            />
+            <div
+              className={classes.searchSuggestions}
+              style={{
+                display: IsInputActive ? 'block' : 'none',
+              }}
+            >
+              <ul>
+                <div className={classes.trendingList}>
+                  <li className={classes.trendingHeading}>
+                    Innovision Techfest
+                  </li>
+                </div>
+                <div className={classes.trendingList}>
+                  <li className={classes.trendingHeading}>Innovision 2019</li>
+                </div>
+                <div className={classes.trendingList}>
+                  <TrendingUpSharpIcon className={classes.trendingSymbol} />
+                  <li className={classes.trendingHeading}>Innovision 2021</li>
+                </div>
+                <div className={classes.trendingList}>
+                  <li className={classes.trendingHeading}>
+                    Innovsion Post-Fest
+                  </li>
+                </div>
+                <div className={classes.trendingList}>
+                  <li className={classes.trendingHeading}>
+                    Innovision Pre-Fest
+                  </li>
+                </div>
+                <div className={classes.trendingList}>
+                  <TrendingUpSharpIcon className={classes.trendingSymbol} />
+                  <li className={classes.trendingHeading}>
+                    Innovision Pro-shows
+                  </li>
+                </div>
+              </ul>
+            </div>
+          </div>
+
+          <div className={classes.trendingArticles}>
+            <TrendingUpSharpIcon />
+            <h3 className={classes.trendingArticleHeading}>Trending Tags :</h3>
+            <h3 className={classes.trendingArticleName}>Departments</h3>
+            <h3 className={classes.trendingArticleName}>Clubs</h3>
+            <h3 className={classes.trendingArticleName}>SAC</h3>
+            <h3 className={classes.trendingArticleName}>Interview</h3>
+            <h3 className={classes.trendingArticleName}>Placement</h3>
+          </div>
+        </div>
+      </div>
+      <Container>
+        <nav
+          aria-label='Monday Morning Navigation'
+          className={classes.navContainer}
         >
-          <div className={classes.imgContainer}>
-            <Image
-              src={logoFullBlack}
-              alt='Monday Morning Logo'
-              className={classes.img}
-              layout='fill'
-              objectFit='cover'
+          <div
+            aria-label='Details Container'
+            className={classes.detailsContainer}
+          >
+            <div className={classes.imgContainer}>
+              <Image
+                src={logoFullBlack}
+                alt='Monday Morning Logo'
+                className={classes.img}
+                layout='fill'
+                objectFit='cover'
+              />
+            </div>
+
+            <TextField
+              variant='standard'
+              label='Search for articles'
+              placeholder='Enter related words'
+              value={search}
+              onKeyDown={searchKeyword}
+              onChange={searchQuery}
+              onClick={searchActive}
             />
           </div>
 
-          <TextField
-            variant='standard'
-            label='Search for articles'
-            placeholder='Enter related words'
-            value={search}
-            onKeyDown={searchKeyword}
-            onChange={searchQuery}
-          />
-        </div>
-
-        <ul aria-label='Navbar' className={classes.menuContainer}>
-          {ROUTES.CATEGORIES.map(({ name, shortName, path }) => (
-            <li key={shortName} className={classes.linkContainer}>
-              <Link
-                className={classes.link}
-                aria-label='Nav Item'
-                href={path}
-                passHref
-              >
-                <Typography variant='h2' className={`${classes.menu}`}>
-                  {name}
-                </Typography>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </Container>
+          <ul aria-label='Navbar' className={classes.menuContainer}>
+            {ROUTES.CATEGORIES.map(({ name, shortName, path }) => (
+              <li key={shortName} className={classes.linkContainer}>
+                <Link
+                  className={classes.link}
+                  aria-label='Nav Item'
+                  href={path}
+                  passHref
+                >
+                  <Typography variant='h2' className={`${classes.menu}`}>
+                    {name}
+                  </Typography>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </Container>
+    </>
   );
 };
 
@@ -134,5 +228,74 @@ const useStyles = makeStyles((theme) => ({
   },
   menu: {
     textAlign: 'center',
+  },
+  searchMenu: {
+    zIndex: '2001',
+    width: '100%',
+    height: '100%',
+    transition: '1s',
+  },
+  searchBar: {
+    paddingLeft: '120px',
+    paddingRight: '120px',
+    position: 'absolute',
+    display: 'block',
+    marginRight: 'auto',
+    width: '100%',
+    paddingBottom: '20px',
+    paddingTop: '60px',
+    background: '#FEFEFF',
+    zIndex: '2000',
+  },
+  searchBox: {
+    position: 'relative',
+  },
+  searchField: {
+    width: '100%',
+    color: theme.palette.primary.blue50,
+    paddingBottom: '0px',
+  },
+  trendingArticles: {
+    position: 'absoulte',
+    display: 'flex',
+    alignItems: 'center',
+    paddingTop: '20px',
+  },
+  trendingArticleHeading: {
+    paddingLeft: '10px',
+    opacity: '0.5',
+    zIndex: '2001',
+  },
+  trendingArticleName: {
+    paddingLeft: '10px',
+  },
+  trendingSymbol: {
+    position: 'absolute',
+  },
+  searchSuggestions: {
+    position: 'absolute',
+    background: '#FEFEFF',
+    width: '100%',
+    padding: '20px',
+    zIndex: '20022',
+    borderRadius: '0px 0px 5px 5px',
+    border: '1px #ECEDEC',
+    borderStyle: 'none solid solid',
+    boxShadow: '0px 0px 5px grey',
+  },
+  trendingList: {
+    alignItems: 'center',
+    paddingBottom: '8px',
+    paddingTop: '8px',
+  },
+  trendingHeading: {
+    paddingLeft: '50px',
+  },
+  blackBackground: {
+    position: 'fixed',
+    background: '#000',
+    width: '100%',
+    height: '100vh',
+    opacity: '0.7',
   },
 }));
