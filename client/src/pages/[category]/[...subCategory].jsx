e11a79e91d68ee21c380e5b9e588e09a105b89b0
Aashish <ashishpradhan2345@gmail.com>
2022-10-28 23:08:17 +0530

    chore: remove to lower case from route

--- client/src/pages/[category]/[...subCategory].jsx
+++ client/src/pages/[category]/[...subCategory].jsx
@@ -37,7 +37,7 @@ const CategoryPage = ({
     setLoading((_val) => true);
 
     push(
-      `/${categoryName?.toLowerCase()}/${subCategoryDetails?.shortName?.toLowerCase()}/${
+      `/${categoryName}/${subCategoryDetails?.shortName}/${
         pageNo ?? pageNumber
       }`,
       undefined,
