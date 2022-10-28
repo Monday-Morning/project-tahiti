9ffd76a50beda03c6ac457d74ffc2c539f36b358
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-10-28 14:16:19 +0530

    chore(env): update env variables

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -59,7 +59,7 @@ const AuthState = ({ children }) => {
           publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
           urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URLENDPOINT,
           authenticationEndpoint:
-            process.env.NEXT_PUBLIC_AUTHENTICATION_ENDPOINT,
+            process.env.NEXT_PUBLIC_IMAGEKIT_AUTHENTICATION_ENDPOINT,
         });
 
         const userPicture = await (await fetch(user.photoURL)).blob();
