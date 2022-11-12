821f29a56d8e7cb2d1a20a18008c1bd32c0b4dbf
Aashish <ashishpradhan2345@gmail.com>
2022-11-12 14:03:45 +0530

    chore: fix indentation

--- client/src/graphql/queries/article/listArticleByYearAndMonth.js
+++ client/src/graphql/queries/article/listArticleByYearAndMonth.js
@@ -4,10 +4,10 @@ import basicArticleData from '../../fragments/article/basicArticleData';
 
 const listArticlesByYearAndMonth = gql`
     query ListArticlesByYearAndMonth($onlyPublished: Boolean!, $limit: Int, $offset: Int, $year: Int, $month: Int) {
-  listArticlesByYearAndMonth(onlyPublished: $onlyPublished, limit: $limit, offset: $offset, year: $year, month: $month) {
-        ${basicArticleData}
-  }
-}
+        listArticlesByYearAndMonth(onlyPublished: $onlyPublished, limit: $limit, offset: $offset, year: $year, month: $month) {
+          ${basicArticleData}
+        }
+    }
 `;
 
 export default listArticlesByYearAndMonth;
