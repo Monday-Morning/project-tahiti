a0e6e81260bdf7a365b8bfbb82655711f8fc1dc7
Aashish <ashishpradhan2345@gmail.com>
2022-12-04 18:46:23 +0530

    feat: add category card

---
+++ client/src/components/admin_v2/AddNew/CategoryCard.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { Typography } from '@mui/material';
+
+export default function CategoryCard({ explorer }) {
+  const [expand, setExpand] = useState(explorer.name ? false : true);
+  return (
+    <>
+      {explorer.name && (
+        <span>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <Checkbox />
+            <Typography
+              onClick={() => setExpand((exp) => !exp)}
+              style={{
+                fontWeight: explorer.items.length > 0 ? 'bolder' : 'normal',
+              }}
+            >
+              {explorer.name}
+            </Typography>
+          </div>
+        </span>
+      )}
+      <div style={{ display: expand ? 'block' : 'none', paddingLeft: '20px' }}>
+        {explorer.items.map((item) => {
+          return <CategoryCard explorer={item} key={item.id} />;
+        })}
+      </div>
+    </>
+  );
+}
