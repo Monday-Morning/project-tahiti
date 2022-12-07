c02c2fdbd13876691f2b8907a5a7ff66423d67f1
Aashish <ashishpradhan2345@gmail.com>
2022-12-07 22:28:38 +0530

    feat: add authors card

---
+++ client/src/components/admin_v2/AddNew/AuthorCards.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {
+  Card,
+  FormControl,
+  Typography,
+  Autocomplete,
+  TextField,
+} from '@mui/material';
+
+export default function AuthorsCard({ title, users, setTeam }) {
+  return (
+    <>
+      <div>
+        <Typography>{title}</Typography>
+        <FormControl
+          component='paper'
+          sx={{ width: '100', minWidth: '100%', margin: '20px 0' }}
+        >
+          <Autocomplete
+            multiple
+            id='multiple-limit-tags'
+            options={users}
+            isOptionEqualToValue={(option, value) => option.id === value.id}
+            renderOption={(props, users) => (
+              <li {...props} key={users.id}>
+                {users.fullName}
+              </li>
+            )}
+            onChange={(e, value) => {
+              setTeam(value);
+              console.log(value);
+            }}
+            renderInput={(params) => (
+              <TextField required={true} {...params} label='Name' />
+            )}
+          />
+        </FormControl>
+      </div>
+    </>
+  );
+}
