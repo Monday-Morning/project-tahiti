e4b3dcdfc1c89f37683f755f4d25ce2d7a98c5d0
Kanhaiya <kagrawal88628@gmail.com>
2022-12-09 11:40:06 +0530

    chore: rename about to event.

--- client/src/pages/events.jsx
+++ client/src/pages/events.jsx
@@ -5,33 +5,33 @@ import Head from 'next/head';
 import Marginals from '../components/marginals/Marginals';
 import EventCalendar from '../components/calendar/EventCalendar';
 
-const AboutPage = () => {
+const EventPage = () => {
   return (
     <>
       <Head>
         {/* <!-- =============== Primary Meta Tags =============== --> */}
-        <title>About | Monday Morning</title>
-        <meta name='title' content='About | Monday Morning' />
+        <title>Event | Monday Morning</title>
+        <meta name='title' content='Event | Monday Morning' />
         <meta
           name='description'
           content='Monday Morning is the official Student Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday Morning also serves as a link between the administration and the students.'
         />
         <meta
           name='keywords'
-          content='about, monday morning, mondaymorning, monday morning, mm, nit rkl, nit, nit rourkela, nitr, nitrkl, rkl, rourkela'
+          content='event, monday morning, mondaymorning, monday morning, mm, nit rkl, nit, nit rourkela, nitr, nitrkl, rkl, rourkela'
         />
 
         {/* <!-- =============== Open Graph / Facebook =============== --> */}
         <meta property='og:type' content='website' />
         <meta
           property='og:url'
-          content='https://mondaymorning.nitrkl.ac.in/about'
+          content='https://mondaymorning.nitrkl.ac.in/event'
         />
         <meta
           property='og:site_name'
           content='Monday Morning | The Student Media Body of NIT Rourkela, India'
         />
-        <meta property='og:title' content='About | Monday Morning' />
+        <meta property='og:title' content='Event | Monday Morning' />
         <meta
           property='og:description'
           content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
@@ -55,7 +55,7 @@ const AboutPage = () => {
         <meta property='twitter:card' content='summary_large_image' />
         <meta
           property='twitter:url'
-          content='https://mondaymorning.nitrkl.ac.in/about'
+          content='https://mondaymorning.nitrkl.ac.in/event'
         />
         <meta property='twitter:title' content='Monday Morning' />
         <meta
@@ -83,4 +83,4 @@ export async function getServerSideProps() {
   };
 }
 
-export default AboutPage;
+export default EventPage;
