03325f89ab7afa6240ac12c5883cb1dd6a38407e
G.Anish Kumar Patro <105335549+anish-patro@users.noreply.github.com>
2022-12-11 01:06:18 +0530

    feat(home): integrate photostory and witsdom (#340)
    * feat(home): integrate photostory and witsdom
    
    * style(home): banner of photostory and witsdom

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -11,7 +11,7 @@ import ArticleCardStack from '../components/widgets/article/ArticleCardStack';
 // import Calendar from '../components/homepage/Calendar';
 import Banner from '../components/homepage/Banner';
 import Trending from '../components/homepage/Trending';
-import SocialMedia from '../components/homepage/SocialMedia';
+// import SocialMedia from '../components/homepage/SocialMedia';
 import ArticleGrid from '../components/widgets/article/ArticleGrid';
 
 function Home({ issues, squiggles, witsdom, photostory, youtubeLink }) {
@@ -67,10 +67,10 @@ function Home({ issues, squiggles, witsdom, photostory, youtubeLink }) {
       <Container>
         <Banner photostory={photostory} witsdom={witsdom} />
 
-        {/* <div style={{ marginTop: 35 }}>
-          <SocialMedia youtubeLink={youtubeLink} />
-          <Trending />
-        </div> */}
+        {/* <div style={{ marginTop: 35 }}> */}
+        {/* <SocialMedia youtubeLink={youtubeLink} /> */}
+        <Trending />
+        {/* </div> */}
       </Container>
     </>
   );
