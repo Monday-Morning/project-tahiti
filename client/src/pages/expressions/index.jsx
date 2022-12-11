badb653a89b25b8277a1e4d1893ff711adf299bd
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2022-12-11 12:46:00 +0530

    fix: add meta data for 500 page, and user portfolio page

--- client/src/pages/expressions/index.jsx
+++ client/src/pages/expressions/index.jsx
@@ -10,7 +10,70 @@ import Custom500 from '../500';
 
 const ExpressionsPage = ({ witsdom, editorial, miscellaneous, isError }) => {
   if (isError) {
-    return <Custom500 />;
+    return (
+      <>
+        <Head>
+          {/* <!-- =============== Primary Meta Tags =============== --> */}
+          <title> Monday Morning </title>
+          <meta name='title' content={`Monday Morning`} />
+          <meta
+            name='description'
+            content='Monday Morning is the official Student Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday Morning also serves as a link between the administration and the students.'
+          />
+          <meta
+            name='keywords'
+            content={`monday morning, mondaymorning, monday morning, mm, nit rkl, nit, nit rourkela, nitr, nitrkl, rkl, rourkela`}
+          />
+
+          {/* <!-- =============== Open Graph / Facebook =============== --> */}
+          <meta property='og:type' content='website' />
+          <meta
+            property='og:url'
+            content={`https://mondaymorning.nitrkl.ac.in/`}
+          />
+          <meta
+            property='og:site_name'
+            content='Monday Morning | The Student Media Body of NIT Rourkela, India'
+          />
+          <meta property='og:title' content={`Monday Morning`} />
+          <meta
+            property='og:description'
+            content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
+          />
+          <meta
+            property='og:image'
+            itemProp='image'
+            content='/icon-256x256.png'
+          />
+          <meta
+            property='og:image:url'
+            content='https://mondaymorning.nitrkl.ac.in/icon-256x256.png'
+          />
+          <meta
+            property='og:image:secure_url'
+            content='https://mondaymorning.nitrkl.ac.in/icon-256x256.png'
+          />
+          <meta property='og:image:type' content='image/png' />
+
+          {/* <!-- =============== Twitter =============== --> */}
+          <meta property='twitter:card' content='summary_large_image' />
+          <meta
+            property='twitter:url'
+            content={`https://mondaymorning.nitrkl.ac.in`}
+          />
+          <meta property='twitter:title' content='Monday Morning' />
+          <meta
+            property='twitter:image'
+            content='https://mondaymorning.nitrkl.ac.in/icon-256x256.png'
+          />
+          <meta
+            property='twitter:description'
+            content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
+          />
+        </Head>
+        <Custom500 />
+      </>
+    );
   }
 
   return (
