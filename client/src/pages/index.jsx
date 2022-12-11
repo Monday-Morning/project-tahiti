728fe76141a7acb2739a6c50ef74c14d7a9780af
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2022-12-11 14:43:37 +0530

    fix: add exception handling to rendering (#319)
    * fix: add exception handling to rendering
    
    * fix: render 500 on error
    
    * chore: remove redundant comments
    
    * chore: remove redundant comments
    
    * chore: remove notFound
    
    * chore: remove isError
    
    * feat(home): integrate photostory and witsdom (#340)
    
    * feat(home): integrate photostory and witsdom
    
    * style(home): banner of photostory and witsdom
    
    * fix: add exception handling to rendering
    
    * fix: add meta data for 500 page, and user portfolio page
    
    * chore: prettier check
    
    Co-authored-by: G.Anish Kumar Patro <105335549+anish-patro@users.noreply.github.com>

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -14,9 +14,82 @@ import Home from '../screens/Home';
 import getLatestIssues from '../graphql/queries/homepage/getLatestIssues';
 import getLatestSquiggle from '../graphql/queries/homepage/getLatestSquiggle';
 import getArticlesByCategories from '../graphql/queries/category/getArticlesByCategories';
+import Custom500 from './500';
 
-function HomePage({ issues, squiggles, witsdom, photostory }) {
+function HomePage({ issues, squiggles, witsdom, photostory, isError }) {
   const { isFallback } = useRouter();
+
+  if (isError) {
+    return (
+      <>
+        <Head>
+          {/* <!-- =============== Primary Meta Tags =============== --> */}
+          <title>
+            Monday Morning | The Student Media Body of NIT Rourkela, India
+          </title>
+          <meta
+            name='title'
+            content='Monday Morning | The Student Media Body of NIT Rourkela, India'
+          />
+          <meta
+            name='description'
+            content='Monday Morning is the official Student Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday Morning also serves as a link between the administration and the students.'
+          />
+          <meta
+            name='keywords'
+            content='monday morning, mondaymorning, monday morning, mm, nit rkl, nit, nit rourkela, nitr, nitrkl, rkl, rourkela'
+          />
+
+          {/* <!-- =============== Open Graph / Facebook =============== --> */}
+          <meta property='og:type' content='website' />
+          <meta
+            property='og:url'
+            content='https://mondaymorning.nitrkl.ac.in/'
+          />
+          <meta
+            property='og:site_name'
+            content='Monday Morning | The Student Media Body of NIT Rourkela, India'
+          />
+          <meta property='og:title' content='Monday Morning' />
+          <meta
+            property='og:description'
+            content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
+          />
+          <meta
+            property='og:image'
+            itemProp='image'
+            content='/icon-256x256.png'
+          />
+          <meta
+            property='og:image:url'
+            content='https://mondaymorning.nitrkl.ac.in/icon-256x256.png'
+          />
+          <meta
+            property='og:image:secure_url'
+            content='https://mondaymorning.nitrkl.ac.in/icon-256x256.png'
+          />
+          <meta property='og:image:type' content='image/png' />
+
+          {/* <!-- =============== Twitter =============== --> */}
+          <meta property='twitter:card' content='summary_large_image' />
+          <meta
+            property='twitter:url'
+            content='https://mondaymorning.nitrkl.ac.in/'
+          />
+          <meta property='twitter:title' content='Monday Morning' />
+          <meta
+            property='twitter:image'
+            content='https://mondaymorning.nitrkl.ac.in/icon-256x256.png'
+          />
+          <meta
+            property='twitter:description'
+            content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
+          />
+        </Head>
+        <Custom500 />;
+      </>
+    );
+  }
 
   return (
     <>
@@ -98,58 +171,66 @@ function HomePage({ issues, squiggles, witsdom, photostory }) {
 }
 
 export async function getStaticProps({ preview }) {
-  const {
-    data: { getLatestIssues: issues },
-  } = await GraphClient.query({
-    query: getLatestIssues,
-    variables: { limit: 2 },
-  });
+  try {
+    const {
+      data: { getLatestIssues: issues },
+    } = await GraphClient.query({
+      query: getLatestIssues,
+      variables: { limit: 2 },
+    });
 
-  if (!issues || issues.length !== 2) {
+    if (!issues || issues.length !== 2) {
+      return {
+        redirect: {
+          destination: '/error/internal-server-error',
+          permanent: false,
+        },
+      };
+    }
+
+    const {
+      data: { getLatestSquiggle: squiggles },
+    } = await GraphClient.query({
+      query: getLatestSquiggle,
+    });
+
+    const {
+      data: {
+        getArticlesByCategories: [witsdom],
+      },
+    } = await GraphClient.query({
+      query: getArticlesByCategories,
+      variables: { categoryNumbers: 61, limit: 1 },
+    });
+
+    const {
+      data: {
+        getArticlesByCategories: [photostory],
+      },
+    } = await GraphClient.query({
+      query: getArticlesByCategories,
+      variables: { categoryNumbers: 62, limit: 1 },
+    });
+
     return {
-      redirect: {
-        destination: '/error/internal-server-error',
-        permanent: false,
+      props: {
+        issues,
+        squiggles,
+        witsdom,
+        photostory,
+      },
+      revalidate:
+        preview || new Date(Date.now()).getDay() < 3
+          ? 60 * 60 * 1
+          : 60 * 60 * 24 * 2, // 1 Hour or 2 Days
+    };
+  } catch (err) {
+    return {
+      props: {
+        isError: true,
       },
     };
   }
-
-  const {
-    data: { getLatestSquiggle: squiggles },
-  } = await GraphClient.query({
-    query: getLatestSquiggle,
-  });
-
-  const {
-    data: {
-      getArticlesByCategories: [witsdom],
-    },
-  } = await GraphClient.query({
-    query: getArticlesByCategories,
-    variables: { categoryNumbers: 61, limit: 1 },
-  });
-
-  const {
-    data: {
-      getArticlesByCategories: [photostory],
-    },
-  } = await GraphClient.query({
-    query: getArticlesByCategories,
-    variables: { categoryNumbers: 62, limit: 1 },
-  });
-
-  return {
-    props: {
-      issues,
-      squiggles,
-      witsdom,
-      photostory,
-    },
-    revalidate:
-      preview || new Date(Date.now()).getDay() < 3
-        ? 60 * 60 * 1
-        : 60 * 60 * 24 * 2, // 1 Hour or 2 Days
-  };
 }
 
 export default HomePage;
