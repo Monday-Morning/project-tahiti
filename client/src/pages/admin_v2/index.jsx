8bab280fd2ad9e34893a48178507d6fce7a247c6
Aashish <ashishpradhan2345@gmail.com>
2022-12-13 17:13:10 +0530

    feat: check permission

--- client/src/pages/admin_v2/index.jsx
+++ client/src/pages/admin_v2/index.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import Paperbase from '../../components/admin_v2/Marginals/Marginals';
 
+import getAccess from '../../utils/getAccess';
+
 const Admin = () => {
   return <Paperbase />;
 };
 
 export default Admin;
+
+export async function getServerSideProps(ctx) {
+  const requiredPermissions = ['article.write.new'];
+  const userPermissions = await getAccess({ ctx });
+
+  const acsessPermitted = requiredPermissions.every((permission) => {
+    return userPermissions.data.includes(permission);
+  });
+
+  if (!acsessPermitted) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { userPermissions }, // will be passed to the page component as props
+  };
+}
