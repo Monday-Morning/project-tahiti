0028ad2231772f84c9dec4f3e303559791cdfe8a
Aashish <ashishpradhan2345@gmail.com>
2022-12-13 17:34:47 +0530

    feat: add error handling

--- client/src/pages/admin_v2/index.jsx
+++ client/src/pages/admin_v2/index.jsx
@@ -1,32 +1,53 @@
 import React from 'react';
+
 import Paperbase from '../../components/admin_v2/Marginals/Marginals';
+import Custom500 from '../500.jsx';
 
 import getAccess from '../../utils/getAccess';
 
-const Admin = () => {
+const Admin = ({ isError }) => {
+  if (isError) {
+    return <Custom500 />;
+  }
+
   return <Paperbase />;
 };
 
 export default Admin;
 
 export async function getServerSideProps(ctx) {
-  const requiredPermissions = ['article.write.new'];
-  const userPermissions = await getAccess({ ctx });
+  try {
+    const requiredPermissions = ['article.write.new'];
+    const userPermissions = await getAccess({ ctx });
 
-  const acsessPermitted = requiredPermissions.every((permission) => {
-    return userPermissions.data.includes(permission);
-  });
+    if (!userPermissions.data) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      };
+    }
 
-  if (!acsessPermitted) {
+    const acsessPermitted = requiredPermissions.every((permission) => {
+      return userPermissions?.data?.includes(permission);
+    });
+
+    if (!acsessPermitted) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      };
+    }
+
     return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
+      props: { userPermissions }, // will be passed to the page component as props
+    };
+  } catch {
+    return {
+      isError: true,
     };
   }
-
-  return {
-    props: { userPermissions }, // will be passed to the page component as props
-  };
 }
