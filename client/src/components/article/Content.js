36583934cf0b63d4e93c9d169f165be2754c461f
anish <anishkumarpatro03@gmail.com>
2022-12-14 12:19:35 +0530

    fix(article): align text

--- client/src/components/article/Content.js
+++ client/src/components/article/Content.js
@@ -140,6 +140,7 @@ const useStyles = makeStyles((theme) => ({
   para: {
     marginTop: '1rem',
     marginBottom: '1rem',
+    textAlign: 'justify',
     hyphens: 'auto',
     fontWeight: 'normal',
   },
