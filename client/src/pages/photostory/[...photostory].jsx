8cf95786473792080a08bbc3d755af62ab280cd2
Anish Sarawgi <98693953+Anish-Sarawgi@users.noreply.github.com>
2022-12-20 16:14:50 +0530

    feat: create individual Photostory page (#331)
    * feat: create individual photostory page
    
    * feat: create individual photostory page
    
    * feat: add new Carousel and Photostory integration
    
    * chore: remove unused codes
    
    * fix: dynamic tags
    
    * fix: redirect to photostory page
    
    * fix: add exception handling to rendering
    
    * fix: render page 500 on error
    
    * refactor: update props name

--- client/src/pages/photostory/[...photostory].jsx
+++ client/src/pages/photostory/[...photostory].jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 
-//liberaries
+// Libraries
 import { GraphClient } from '../../config/ApolloClient';
 
-//components
-import Archive from '../../screens/Archive';
-import ActivityIndicator from '../../components/shared/ActivityIndicator';
+// Components
 import Marginals from '../../components/marginals/Marginals';
+import Photostory from '../../screens/Photostory';
+import ActivityIndicator from '../../components/shared/ActivityIndicator';
 
-//gql
-import listArticlesByYearAndMonth from '../../graphql/queries/article/listArticleByYearAndMonth';
-
-//routes
-import { ARCHIVES } from '../../assets/placeholder/guide';
+// Queries
+import getArticleByID from '../../graphql/queries/article/getArticleByID';
+import getArticleLink, { getArticleSlug } from '../../utils/getArticleLink';
 import Custom500 from '../500';
 
-function ArchivePage({ archiveArticles, year, month, isError }) {
+function PhotostoryPage({ photostory, isError }) {
   const { isFallback } = useRouter();
 
   if (isError) {
@@ -25,28 +23,28 @@ function ArchivePage({ archiveArticles, year, month, isError }) {
       <>
         <Head>
           {/* <!-- =============== Primary Meta Tags =============== --> */}
-          <title> Monday Morning </title>
-          <meta name='title' content={`Monday Morning`} />
+          <title>Photostory | Monday Morning</title>
+          <meta name='title' content='Photostory | Monday Morning' />
           <meta
             name='description'
             content='Monday Morning is the official Student Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday Morning also serves as a link between the administration and the students.'
           />
           <meta
             name='keywords'
-            content={`monday morning, mondaymorning, monday morning, mm, nit rkl, nit, nit rourkela, nitr, nitrkl, rkl, rourkela`}
+            content='photostory, monday morning, mondaymorning, monday morning, mm, nit rkl, nit, nit rourkela, nitr, nitrkl, rkl, rourkela'
           />
 
           {/* <!-- =============== Open Graph / Facebook =============== --> */}
           <meta property='og:type' content='website' />
           <meta
             property='og:url'
-            content={`https://mondaymorning.nitrkl.ac.in/`}
+            content='https://mondaymorning.nitrkl.ac.in/photostory'
           />
           <meta
             property='og:site_name'
             content='Monday Morning | The Student Media Body of NIT Rourkela, India'
           />
-          <meta property='og:title' content={`Monday Morning`} />
+          <meta property='og:title' content='Photostory | Monday Morning' />
           <meta
             property='og:description'
             content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
@@ -70,7 +68,7 @@ function ArchivePage({ archiveArticles, year, month, isError }) {
           <meta property='twitter:card' content='summary_large_image' />
           <meta
             property='twitter:url'
-            content={`https://mondaymorning.nitrkl.ac.in`}
+            content='https://mondaymorning.nitrkl.ac.in/photostory'
           />
           <meta property='twitter:title' content='Monday Morning' />
           <meta
@@ -82,7 +80,7 @@ function ArchivePage({ archiveArticles, year, month, isError }) {
             content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
           />
         </Head>
-        <Custom500 />
+        <Custom500 />;
       </>
     );
   }
@@ -91,28 +89,28 @@ function ArchivePage({ archiveArticles, year, month, isError }) {
     <>
       <Head>
         {/* <!-- =============== Primary Meta Tags =============== --> */}
-        <title>Archive | Monday Morning</title>
-        <meta name='title' content='Archive | Monday Morning' />
+        <title>Photostory | Monday Morning</title>
+        <meta name='title' content='Photostory | Monday Morning' />
         <meta
           name='description'
           content='Monday Morning is the official Student Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday Morning also serves as a link between the administration and the students.'
         />
         <meta
           name='keywords'
-          content='archive, monday morning, mondaymorning, monday morning, mm, nit rkl, nit, nit rourkela, nitr, nitrkl, rkl, rourkela'
+          content='photostory, monday morning, mondaymorning, monday morning, mm, nit rkl, nit, nit rourkela, nitr, nitrkl, rkl, rourkela'
         />
 
         {/* <!-- =============== Open Graph / Facebook =============== --> */}
         <meta property='og:type' content='website' />
         <meta
           property='og:url'
-          content='https://mondaymorning.nitrkl.ac.in/archive'
+          content='https://mondaymorning.nitrkl.ac.in/photostory'
         />
         <meta
           property='og:site_name'
           content='Monday Morning | The Student Media Body of NIT Rourkela, India'
         />
-        <meta property='og:title' content='Archive | Monday Morning' />
+        <meta property='og:title' content='Photostory | Monday Morning' />
         <meta
           property='og:description'
           content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
@@ -136,7 +134,7 @@ function ArchivePage({ archiveArticles, year, month, isError }) {
         <meta property='twitter:card' content='summary_large_image' />
         <meta
           property='twitter:url'
-          content='https://mondaymorning.nitrkl.ac.in/archive'
+          content='https://mondaymorning.nitrkl.ac.in/photostory'
         />
         <meta property='twitter:title' content='Monday Morning' />
         <meta
@@ -148,43 +146,55 @@ function ArchivePage({ archiveArticles, year, month, isError }) {
           content='Monday Morning is the Media Body of National Institute Of Technology Rourkela. Monday Morning covers all the events, issues and activities going on inside the campus. Monday morning also serves as a link between the administration and the students.'
         />
       </Head>
-      <Marginals>
-        {isFallback ? (
-          <ActivityIndicator size={150} />
-        ) : (
-          <Archive
-            archiveArticles={archiveArticles}
-            year={parseInt(year)}
-            month={month}
-          />
-        )}
-      </Marginals>
+      {isFallback || !photostory ? (
+        <ActivityIndicator size={150} />
+      ) : (
+        <Marginals>
+          <Photostory photostory={photostory} />
+        </Marginals>
+      )}
     </>
   );
 }
 
-export default ArchivePage;
-
 export async function getStaticProps({
   params: {
-    yearAndMonth: [year, month],
+    photostory: [photostoryId, photostorySlug],
   },
+  preview,
 }) {
   try {
     const {
-      data: { listArticlesByYearAndMonth: archiveArticles },
+      data: { getArticleByID: photostory },
     } = await GraphClient.query({
-      query: listArticlesByYearAndMonth,
-      variables: {
-        onlyPublished: true,
-        year: parseInt(year),
-        month: ARCHIVES.months.indexOf(month),
-        limit: 9,
-      },
+      query: getArticleByID,
+      variables: { id: photostoryId },
     });
+
+    if (!photostory) {
+      return {
+        notFound: true,
+      };
+    }
+
+    if (photostorySlug !== getArticleSlug(photostory.title)) {
+      return {
+        redirect: {
+          destination: getArticleLink(photostoryId, photostory.title),
+          permanent: false,
+        },
+      };
+    }
+
     return {
-      props: { archiveArticles, year, month },
-      revalidate: 7 * 24 * 60 * 60,
+      props: {
+        key: photostoryId,
+        photostory,
+      },
+      revalidate:
+        preview || photostory.publishStatus === 'PUBLISHED'
+          ? 10
+          : 60 * 60 * 24 * 30, // 30 Days
     };
   } catch (err) {
     return {
@@ -196,29 +206,10 @@ export async function getStaticProps({
 }
 
 export async function getStaticPaths() {
-  const { years, months } = ARCHIVES;
-
-  try {
-    const paths = years
-      .map((year) =>
-        months.map((month) => {
-          return {
-            params: {
-              yearAndMonth: [`${year}`, month],
-            },
-          };
-        }),
-      )
-      .flat();
-
-    return {
-      paths,
-      fallback: true,
-    };
-  } catch (e) {
-    return {
-      paths: { params: { subCategory: ['error', 'error'] } },
-      fallback: true,
-    };
-  }
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
 }
+
+export default PhotostoryPage;
