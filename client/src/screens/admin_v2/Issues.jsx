54da316ab77b2d0da0d7bf8638b7cb3ebca9583a
Aashish <ashishpradhan2345@gmail.com>
2023-01-01 14:33:58 +0530

    feat: add issues screen

--- client/src/screens/admin_v2/Issues.jsx
+++ client/src/screens/admin_v2/Issues.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+//graphql
+import { GraphClient } from '../../config/ApolloClient';
+import getIssueById from '../../graphql/queries/issues/getIssueById';
+import removeIssue from '../../graphql/mutations/issues/removeIssue';
+import getLatestIssues from '../../graphql/queries/issues/getLatestIssues';
+
+//components
 import Marginals from '../../components/admin_v2/Marginals/Marginals';
+import IssueDialogBox from '../../components/admin_v2/issues/IssueDialogBox';
 
 //mui
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TablePagination from '@mui/material/TablePagination';
-import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import EditIcon from '@mui/icons-material/Edit';
@@ -36,15 +43,76 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 const Issues = ({ issues }) => {
+  const [dialogBoxOpen, setDialogBoxOpen] = useState(false);
+  const [seletedIssue, setSelectedIssue] = useState({
+    name: '',
+    articles: [],
+    featured: [],
+  });
+  const [action, setAction] = useState('Add');
+  const [issueList, setIssueList] = useState(issues || []);
+  const [offset, setOffset] = useState(0);
+
+  const getIssueFromId = async (id) => {
+    setAction('Update');
+    setDialogBoxOpen(true);
+    const {
+      data: { getIssueByID: issue },
+    } = await GraphClient.query({
+      query: getIssueById,
+      variables: {
+        getIssueByIdId: id,
+      },
+    });
+    setSelectedIssue(issue);
+  };
+
+  const deleteIssue = async (id) => {
+    alert('Are you sure you want to delete this issue?');
+
+    await GraphClient.mutate({
+      mutation: removeIssue,
+      variables: {
+        removeIssueId: id,
+      },
+    });
+    setIssueList(issueList.filter((issue) => issue.id !== id));
+  };
+
+  const getMoreissues = async () => {
+    const {
+      data: { getLatestIssues: issues },
+    } = await GraphClient.query({
+      query: getLatestIssues,
+      variables: {
+        limit: 9,
+        offset: offset + 10,
+      },
+    });
+    setOffset((prev) => prev + 10);
+    setIssueList((prev) => [...prev, ...issues]);
+  };
+
   return (
     <div>
       <Marginals>
+        <IssueDialogBox
+          seletedIssue={seletedIssue}
+          dialogBoxOpen={dialogBoxOpen}
+          setDialogBoxOpen={setDialogBoxOpen}
+          action={action}
+          issueList={issueList}
+          setIssueList={setIssueList}
+        />
         <Button
           sx={{
             marginBottom: '10px',
           }}
           variant='contained'
           color='primary'
+          onClick={() => {
+            setAction('Add'), setDialogBoxOpen(true);
+          }}
         >
           <AddCircleOutlineIcon sx={{ marginRight: '10px' }} />
           Add Issue
@@ -61,7 +129,7 @@ const Issues = ({ issues }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {issues.map(({ id, name, startDate, endDate, isPublished }) => (
+            {issueList.map(({ id, name, startDate, endDate, isPublished }) => (
               <StyledTableRow>
                 <StyledTableCell scope='row'>{name}</StyledTableCell>
                 <StyledTableCell align='left'>
@@ -72,15 +140,17 @@ const Issues = ({ issues }) => {
                 </StyledTableCell>
                 <StyledTableCell align='center'>Published</StyledTableCell>
                 <StyledTableCell align='center'>
-                  <EditIcon />
+                  <EditIcon onClick={() => getIssueFromId(id)} />
                 </StyledTableCell>
                 <StyledTableCell align='center'>
-                  <DeleteIcon />
+                  <DeleteIcon onClick={() => deleteIssue(id)} />
                 </StyledTableCell>
               </StyledTableRow>
             ))}
           </TableBody>
         </Table>
+
+        <Button onClick={getMoreissues}>Load More</Button>
       </Marginals>
     </div>
   );
