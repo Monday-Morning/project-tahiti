f5806b974c58b17a9318f2a27ee83aa084141189
Amal <amal.mohan.ga@gmail.com>
2023-01-14 17:34:53 +0530

    Hall Images

--- client/src/components/widgets/DropDownCard.js
+++ client/src/components/widgets/DropDownCard.js
@@ -56,5 +56,8 @@ const useStyles = makeStyles((theme) => ({
     lineHeight: '32px',
     color: '#222222',
     paddingTop: '6px',
+    [theme.breakpoints.down('sm')]:{
+      fontSize: '20px',
+    }
   },
 }));
