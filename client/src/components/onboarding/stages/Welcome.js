365b738fba8e1803dc2942d635c0c7d15c55b093
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2023-03-12 14:54:16 +0530

    fix: uncomment useRouter

--- client/src/components/onboarding/stages/Welcome.js
+++ client/src/components/onboarding/stages/Welcome.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 import Image from 'next/image';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
 import makeStyles from '@mui/styles/makeStyles';
 import { Typography } from '@mui/material';
